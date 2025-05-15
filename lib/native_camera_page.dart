3277631a1fe077019f9ec65b0b24d5e7b0d0e5ac
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-05-16 01:00:27 +0700

    Menambahkan tampilan layout kamera.

--- lib/native_camera_page.dart
+++ lib/native_camera_page.dart
@@ -179,8 +179,62 @@ class _CameraPageState extends State<CameraPage> {
   }
 
   @override
+  void dispose() {
+    _controller?.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
+      body:
+          (_controller?.value.isInitialized ?? false)
+              ? LayoutBuilder(
+                builder: (context, constraints) {
+                  return Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      GestureDetector(
+                        onTapDown: (d) => _handleTap(d, constraints),
+                        child: CameraPreview(_controller!),
+                      ),
+                      Positioned(
+                        top: 50,
+                        right: 20,
+                        child: Column(
+                          children: [
+                            _circleButton(
+                              Icons.flip_camera_android,
+                              _switchCamera,
+                            ),
+                            const SizedBox(width: 12),
+                            _circleButton(_flashIcon(), _toggleFlash),
+                          ],
+                        ),
+                      ),
+                      _buildZoomControl(),
+                      Positioned(
+                        bottom: 40,
+                        left: 0,
+                        right: 0,
+                        child: Center(
+                          child: FloatingActionButton(
+                            onPressed: _captureImage,
+                            backgroundColor: Colors.white,
+                            child: const Icon(
+                              Icons.camera_alt,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              )
+              : const Center(child: CircularProgressIndicator()),
     );
   }
 }
